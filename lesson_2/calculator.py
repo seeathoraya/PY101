da255304f18f9d141a2db5266091662a50eb4772
Shamash Athoraya <shamash.athoraya@gmail.com>
2025-04-17 22:36:09 +0200

    Refactoring of calculator.py and added process_students.py

--- lesson_2/calculator.py
+++ lesson_2/calculator.py
@@ -1,15 +1,31 @@
 def prompt(message):
     print(f"==> {message}")
 
+def invalid_number(number_str):
+    try:
+        int(number_str)
+    except ValueError:
+        return True
+
+    return False
+
 prompt("Welcome to Calculator!")
 
 # 1. Ask for the first number
 prompt("What's the first number?")
 number1 = input()
 
+while invalid_number(number1):
+    prompt("Hmm... that doesn't look like a valid number.")
+    number1 = input()
+
 # 2. Ask for the second number
 prompt("What's the second number?")
 number2 = input()
+
+while invalid_number(number2):
+    prompt("Hmm... that doesn't look like a valid number.")
+    number2 = input()
 
 prompt(f"{number1} & {number2}")
 
@@ -17,6 +33,10 @@ prompt(f"{number1} & {number2}")
 prompt("What operation would you like to perform?\n"
       "1) Add \n2) Subtract \n3) Multiply \n4) Divide")
 operation = input()
+
+while operation not in ["1", "2", "3", "4"]:
+    prompt("You must choose 1, 2, 3, or 4")
+    operation = input()
 
 # 4. Carry out calculation
 match operation:
@@ -31,4 +51,3 @@ match operation:
 
 # 5. Print the result
 prompt(f"The result is: {result}")
-
