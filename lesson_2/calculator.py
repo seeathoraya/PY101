a274377c5637c5a25c1c4558bd2a80e78acc4c14
Shamash Athoraya <shamash.athoraya@gmail.com>
2025-04-23 22:34:45 +0200

    Added re-run prompt

--- lesson_2/calculator.py
+++ lesson_2/calculator.py
@@ -9,45 +9,58 @@ def invalid_number(number_str):
 
     return False
 
-prompt("Welcome to Calculator!")
-
-# 1. Ask for the first number
-prompt("What's the first number?")
-number1 = input()
-
-while invalid_number(number1):
-    prompt("Hmm... that doesn't look like a valid number.")
+def run_calculation():
+    # 1. Ask for the first number
+    prompt("What's the first number?")
     number1 = input()
 
-# 2. Ask for the second number
-prompt("What's the second number?")
-number2 = input()
+    while invalid_number(number1):
+        prompt("Hmm... that doesn't look like a valid number.")
+        number1 = input()
 
-while invalid_number(number2):
-    prompt("Hmm... that doesn't look like a valid number.")
+    # 2. Ask for the second number
+    prompt("What's the second number?")
     number2 = input()
 
-prompt(f"{number1} & {number2}")
+    while invalid_number(number2):
+        prompt("Hmm... that doesn't look like a valid number.")
+        number2 = input()
 
-# 3. Ask which operation to perform
-prompt("What operation would you like to perform?\n"
-      "1) Add \n2) Subtract \n3) Multiply \n4) Divide")
-operation = input()
+    prompt(f"{number1} & {number2}")
 
-while operation not in ["1", "2", "3", "4"]:
-    prompt("You must choose 1, 2, 3, or 4")
+    # 3. Ask which operation to perform
+    prompt("What operation would you like to perform?\n"
+        "1) Add \n2) Subtract \n3) Multiply \n4) Divide")
     operation = input()
 
-# 4. Carry out calculation
-match operation:
-    case "1":  # 1 is addition
-        result = int(number1) + int(number2)
-    case "2":  # 2 is subtraction
-        result = int(number1) - int(number2)
-    case "3":  # 3 is multiplication
-        result = int(number1) * int(number2)
-    case "4":  # 4 is division
-        result = int(number1) / int(number2)
+    while operation not in ["1", "2", "3", "4"]:
+        prompt("You must choose 1, 2, 3, or 4")
+        operation = input()
 
-# 5. Print the result
-prompt(f"The result is: {result}")
+    # 4. Carry out calculation
+    match operation:
+        case "1":  # 1 is addition
+            result = int(number1) + int(number2)
+        case "2":  # 2 is subtraction
+            result = int(number1) - int(number2)
+        case "3":  # 3 is multiplication
+            result = int(number1) * int(number2)
+        case "4":  # 4 is division
+            result = int(number1) / int(number2)
+
+    # 5. Print the result
+    prompt(f"The result is: {result}")
+
+prompt("Welcome to Calculator!")
+
+while True:
+    run_calculation()
+    prompt("Do you want to perform another calculation? (y/n)")
+    answer = input()
+
+    while answer not in ["y", "n"]:
+        prompt("You must enter 'y' or 'n'")
+        answer = input()
+
+    if answer == "n":
+        break
